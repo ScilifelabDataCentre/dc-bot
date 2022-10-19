a1930747350429224d8b37c2bfc856b0474186ff
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2022-10-19 13:17:58 +0200

    Change to http mode

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 
 # Initializes your app with your bot token and socket mode handler
-app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
+app = App(token=os.environ.get("SLACK_BOT_TOKEN"),
+          signing_secret=os.environ.get("SLACK_SIGNING_SECRET"))
 
 
 @app.message("Hi dc-bot!")
@@ -13,4 +14,4 @@ def message_hello(message, say):
 
 # Start your app
 if __name__ == "__main__":
-    SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
+    app.start(port=int(os.environ.get("PORT", 3000)))
