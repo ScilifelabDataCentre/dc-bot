2bbf4d2e08ff356a42ce3a8c7ffebaf945c2bf13
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2022-11-25 11:45:00 +0100

    For container instead of dev

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 from slack_bolt import App
-from slack_bolt.adapter.socket_mode import SocketModeHandler
+#from slack_bolt.adapter.socket_mode import SocketModeHandler
 
 app = App(token=os.environ.get("SLACK_BOT_TOKEN"),
           signing_secret=os.environ.get("SLACK_SIGNING_SECRET"))
@@ -35,5 +35,5 @@ def respond_to_channel_join(body, say, logger):
 
 # Start your app
 if __name__ == "__main__":
-#    app.start(port=int(os.environ.get("PORT", 3000)))
-    SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
+    app.start(port=int(os.environ.get("PORT", 3000)))
+#    SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
